377f0fc31dee7ec056df00e9a2d9d0d4d0417389
Yeeun Heo <38968449+yyyeun@users.noreply.github.com>
2025-01-10 20:25:05 +0900

    Update 다른 풀이.py

--- Python3/프로그래머스/1/12928. 약수의 합/다른 풀이.py
+++ Python3/프로그래머스/1/12928. 약수의 합/다른 풀이.py
@@ -1,4 +1,11 @@
+# num/2의 수들만 검사하면 성능 약 2배 향상
 def sumDivisor(num):
-    # num / 2 의 수들만 검사하면 성능 약 2배 향상
     return num + sum([i for i in range(1, (num // 2) + 1) if num % i == 0])
 
+# list comprehension
+def sumDivisor(num):
+    return sum([i for i in range(1,num+1) if num%i==0])
+
+# filter + lambda
+def sumDivisor(num):
+    return sum(filter(lambda x: num % x == 0, range(1, num + 1)))
