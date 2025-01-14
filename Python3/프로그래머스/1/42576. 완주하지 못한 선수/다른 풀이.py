3bdfc3130b2a4c10ff2a1da8b2f0752d52a73734
Yeeun Heo <38968449+yyyeun@users.noreply.github.com>
2025-01-15 01:09:04 +0900

    Create 다른 풀이.py

---
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/다른 풀이.py
@@ -0,0 +1,32 @@
+# 1-1. Counter 객체 사용 (긴 버전)
+from collections import Counter
+
+def solution(participant, completion):
+    count = Counter(participant)
+    for person in completion:
+        count[person] -= 1
+        if count[person] == 0:
+            del count[person]
+    return list(count.keys())[0]
+
+
+# 1-2. Counter 객체 사용 (짧은 버전)
+from collections import Counter
+
+def solution(participant, completion):
+    return list((Counter(participant) - Counter(completion)).keys())[0]
+
+
+# 2. hash 함수 사용
+def solution(participant, completion):
+    hash_dict = {}
+    hash_sum = 0
+
+    for p in participant:
+        hash_dict[hash(p)] = p
+        hash_sum += hash_value
+
+    for c in completion:
+        hash_sum -= hash(c)
+
+    return hash_dict[hash_sum]
