5e18722a526ad344cdc2f44ab2f13ca061cb2cec
Yeeun Heo <38968449+yyyeun@users.noreply.github.com>
2025-01-26 23:23:14 +0900

    [Silver I] Title: 절댓값 힙, Time: 540 ms, Memory: 29492 KB -BaekjoonHub

--- Java/백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ Java/백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,29 +1,32 @@
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2)->{
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+
+            if (first_abs == second_abs) {
+                return o1 < o2 ? -1 : 1;
+            }
+            return first_abs - second_abs;
+        });
 
         for (int i = 0; i < N; i++) {
-            int num = sc.nextInt();
-
-            if (num != 0) {
-                // 배열에 값을 추가
-                if (num > 0) pq1.add(num);
-                else pq2.add(num);
-            } else {
-                // 배열이 비어 있는 경우는 0 출력
-                if (pq1.isEmpty() && pq2.isEmpty()) System.out.println(0);
-                // 배열에서 절댓값이 가장 작은 값을 출력하고 그 값을 배열에서 제거
-                else {
-                    if (!pq1.isEmpty() && (pq2.isEmpty() || pq1.peek() < pq2.peek() * -1)) System.out.println(pq1.poll());
-                    else if (!pq2.isEmpty() && (pq1.isEmpty() || pq1.peek() >= pq2.peek() * -1)) System.out.println(pq2.poll());
+            int request = Integer.parseInt(br.readLine());
+            if (request == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
                 }
+            } else {
+                pq.add(request);
             }
         }
     }
