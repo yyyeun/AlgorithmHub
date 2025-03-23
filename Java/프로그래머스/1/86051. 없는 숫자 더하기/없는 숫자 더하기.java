9b35b03f635822a0e5a20cb1d747e1937cb914b0
Yeeun Heo <38968449+yyyeun@users.noreply.github.com>
2025-03-24 01:14:53 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 1.73 ms, Memory: 83.7 MB -BaekjoonHub

--- Java/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ Java/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,21 +1,16 @@
 import java.util.*;
 
 class Solution {
+    
+    static int i;
+    
     public int solution(int[] numbers) {
         int answer = 0;
         
-        Arrays.sort(numbers);
-        
-        Integer[] all = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        Integer[] numbersI = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
-
-        Set<Integer> allH = new HashSet<>(Arrays.asList(all));
-        Set<Integer> numbersH = new HashSet<>(Arrays.asList(numbersI));
-        
-        allH.removeAll(numbersH);
-        
-        for (Integer i : allH) {
-            answer += Integer.valueOf(i);
+        for (i=0; i<=9; i++) {
+            if(!Arrays.stream(numbers).anyMatch(n -> n == i)) {
+                answer += i;
+            }
         }
         
         return answer;
