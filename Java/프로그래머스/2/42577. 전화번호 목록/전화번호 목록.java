d6ab6f3af91ffffa2d3171ea5cfd599389c87dd2
Yeeun Heo <38968449+yyyeun@users.noreply.github.com>
2025-04-14 18:42:50 +0900

    [level 2] Title: 전화번호 목록, Time: 311.50 ms, Memory: 179 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Set phone_set = new HashSet<String>();
+        
+        for (String p : phone_book) {
+            phone_set.add(p);
+        }
+        
+        for (String p : phone_book) {
+            for (int i=1; i<p.length(); i++) {
+                if (phone_set.contains(p.substring(0, i))) {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
